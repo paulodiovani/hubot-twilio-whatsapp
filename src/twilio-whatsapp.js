2a3392d868322a069b12ff5aedfe5073bfba09c5
Paulo Diovani <paulo@diovani.com>
2019-08-02 17:34:29 -0300

    Add http server

--- src/twilio-whatsapp.js
+++ src/twilio-whatsapp.js
@@ -1,27 +1,57 @@
 const EventEmitter = require('events').EventEmitter
-const { TextMessage, User } = require('hubot')
+const { TextMessage } = require('hubot')
 
 class TwilioWhatsAppAdapter extends EventEmitter {
   constructor (robot) {
     super()
     this.robot = robot
-    this.robot.logger.info('Constructor')
+    this.robot.logger.debug('Constructor')
   }
 
   send (envelope, ...strings) {
-    this.robot.logger.info('Send')
+    this.robot.logger.debug('Send', envelope, strings)
+  }
+
+  emote (envelope, ...strings) {
+    Array.from(strings).map((str) => this.send(envelope, `* ${str}`))
   }
 
   reply (envelope, ...strings) {
-    this.robot.logger.info('Reply')
+    this.robot.logger.debug('Reply')
+    const answers = strings.map((s) => `${envelope.user.name}: ${s}`)
+    this.send(this, envelope, ...answers)
   }
 
+  // reply (envelope, ...strings) {
+  //   this.robot.logger.info('Reply')
+  // }
+
   run () {
-    this.robot.logger.info('Run')
+    this.robot.logger.debug('Run')
+
+    this.robot.router.post('/hubot/sms', (req, res) => {
+      const payload = req.body.payload ? JSON.parse(req.body.payload) : req.body
+      this.robot.logger.debug('Post', payload)
+
+      this.receiveSms(payload, () => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end()
+      })
+    })
+
     this.emit('connected')
-    const user = new User(1001, { name: 'Sample User' })
-    const message = new TextMessage(user, 'Some Sample Message', 'MSG-001')
-    this.robot.receive(message)
+  }
+
+  receiveSms (payload, cb) {
+    const { Body, From, MessageSid = 'messageId' } = payload
+
+    if (!From || !Body.trim()) return
+
+    const user = this.robot.brain.userForId(From, { name: 'Twilio', room: 'Twilio', number: From })
+    const message = new TextMessage(user, Body.trim(), MessageSid)
+    this.robot.logger.debug('Message', message)
+
+    this.robot.receive(message, cb)
   }
 }
 
